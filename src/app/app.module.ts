06d6145d538ffe3ddba19dbe9611cc923c0eda40
luismavlo <lmavendano1597@gmail.com>
2022-01-31 17:16:35 -0500

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
+import { CalculatorComponent } from './calculator/calculator.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    HeaderComponent,
+    CalculatorComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule,
+    FontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/calculator/calculator.component.ts
@@ -0,0 +1,47 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-calculator',
+  templateUrl: './calculator.component.html',
+  styleUrls: ['./calculator.component.css']
+})
+export class CalculatorComponent implements OnInit {
+  answer: number = 0;
+  
+  
+  constructor() { }
+
+  getSum(num1: string, num2: string ): void{
+    this.answer = (+num1) + (+num2);
+  }
+
+  getSubstraction(num1: string, num2: string): void{
+    this.answer = (+num1) - (+num2);
+  }
+
+  getMultiplication(num1: string, num2: string): void{
+    this.answer = (+num1) * (+num2);
+  }
+
+  getDivision(num1: string, num2: string): void{
+    this.answer = (+num1) / (+num2);
+  }
+
+  getEmpowerment(num1: string, num2: string): void{
+    let res: number = 1;
+    let total: number = 0
+    for (let i = 0; i < +num2; i++) {
+        res = +num1* res;    
+    }
+    this.answer = res; 
+  }
+
+  getAnswer(){
+    return this.answer;
+  }
+
+  ngOnInit(): void {
+  }
+
+}
+ 
